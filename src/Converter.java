25a94c2cffbd03b68abb34f7229badf0de90e00e
Faruk Becirovic <faruk_becirovic@outlook.com>
2018-10-17 17:35:28 +0200

    version

--- src/Converter.java
+++ src/Converter.java
@@ -1,4 +1,32 @@
 
 public class Converter {
+	
+	public static double kgToLb(double kg) {
+		return kg;
+	}
+	
+	public static double lbToKg(double lb) {
+		return lb;
+	}
+	
+	public static double cmToInch(double cm) {
+		return cm;
+	}
+	
+	public static double inchToCm(double inch) {
+		return inch;
+	}
+	
+	public static double celsiusToFahrenheit(double c) {
+		return c;
+	}
+	
+	public static double fahrenheitToCelsius(double f) {
+		return f;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
 
 }
